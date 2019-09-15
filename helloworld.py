0ae975aed1e0169c641cffda2338a99fcc52fcd1
batrar <batrar@umich.edu>
2019-09-14 21:08:51 -0400

    Pushing some python code

---
+++ helloworld.py
@@ -0,0 +1,14 @@
+# 1st Lab - Git/Python Intro
+
+from random import randint
+
+a = randint(1,100)
+b = randint(1,100)
+
+result = a + b
+
+print("Rohan Batra")
+print(a)
+print(b)
+print("Sum = " + str(result))
+print("Average = " + str(result/2.0))
